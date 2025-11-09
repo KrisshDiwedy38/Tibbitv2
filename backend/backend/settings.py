0008c06cdf6d53b92746dc9bd819aff7b86ea436
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-11-09 14:44:54 +0530

    users and listings app models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'listings'
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
