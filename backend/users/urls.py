903039a8792b5763ae4895d9d29dd87333d0d97f
Krissh Diwedy <krisshdiwedy38@gmail.com>
2026-07-21 20:42:07 +0530

    4. Background Refresh Endpoint
    Added CookieTokenRefreshView to automatically refresh access tokens using the 7-day refresh cookie.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,10 +6,13 @@ from .views import (
     OTPVerifyView,
     ResendOTPView,
     LoginView,
+    LogoutView,
     UserProfileView,
     WaitlistCreateView,
     ContactFounderView,
-    ReportBugView
+    ReportBugView,
+    PasswordResetRequestView,
+    PasswordResetConfirmView
 )
 
 urlpatterns = [
@@ -18,6 +21,9 @@ urlpatterns = [
     re_path(r'^verify-otp/?$', OTPVerifyView.as_view(), name='verify-otp'),
     re_path(r'^resend-otp/?$', ResendOTPView.as_view(), name='resend-otp'),
     re_path(r'^login/?$', LoginView.as_view(), name='login'),
+    re_path(r'^logout/?$', LogoutView.as_view(), name='logout'),
+    re_path(r'^password-reset/?$', PasswordResetRequestView.as_view(), name='password-reset'),
+    re_path(r'^password-reset-confirm/?$', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
     re_path(r'^token/refresh/?$', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^profile/?$', UserProfileView.as_view(), name='profile'),
     re_path(r'^waitlist/?$', WaitlistCreateView.as_view(), name='waitlist'),
